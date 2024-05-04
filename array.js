e707fc6eff5a86e746c95ecae3ae2eb4aec324ec
Gabriela <54961860+gabrielaspooky@users.noreply.github.com>
2024-05-04 13:42:58 +0200

    started new changes

---
+++ array.js
@@ -0,0 +1,23 @@
+// 1.- Crea una variable que tenga como valor un array vacío.
+
+const userName = []
+
+// 2.- Crea una variable que tenga como valor un array con al menos 3 tipos de datos distintos.
+
+let catProperties = [Fígaro, 12, true]
+
+// 3.- 
+//          3.1.- Crea un array vacío y añádele dos valores usando push(), dos valores usando unshift() y luego usa pop() y shift() una vez. A continuación, muestra por consola la longitud del array.
+//          3.2.- Crea un array classNames con todos los nombres de tus compañer@s de clase. Muestra por consola tu nombre seleccionando el que hay en el array classNames.
+
+// 4.- Crea el siguiente array: const names = ["Carlos", "María", "Isidro", "Elizondo", "Molina"]. A continuación, muestra por consola los nombres ["María, Isidro"] usando slice(). Luego, sustituye "Elizondo" por "Parma" usando splice() y muestra el resultados por consola.
+
+// 5.- Convierte el array anterior (names) en un string de nombres separados por espacios.
+
+// 6.- Crea la variable const sentence = "El perro de San Roque no tiene rabo". A continuación, conviértela en un array de palabras, y, luego, en otro de letras.
+
+// 7.- Diseña un programa (función) que vaya leyendo números y guardándolos en un array. Una vez lleno mostrará el array y deberá decir cuantos números son pares y cuantos son impares. La entrada de datos termina cuando el usuario teclea 0 o un valor no numérico. ( permite detectar valores no numéricos).
+
+// 8.- Un servicio de atención al cliente tiene establecido turnos semanales para sus empleados de manera que cada día de la semana se encarga del servicio una persona: Lunes - María; Martes - Luis; Miércoles - Antonia; Jueves - Pedro; Viernes - Marisa; Sábado y Domingo - Sin Servicio. Usa un array para almacenar los datos del servicio. En este array cada elemento será un par [día - nombre]. Crea una función que responda con el nombre de la persona encargada del servicio sabiendo el día. Si le pido servicio('Lunes') me debería decir "Este día se encarga María" y si le pido servicio(Miércoles") me daría Antonia.
+
+// 9.- Una tienda vende monitores, teclados y ratones. Los precios se almacenan en una estructura array donde cada elemento es un par [producto - precio]. Diseña una función que reciba como argumento el nombre de un producto (minúscula o mayúscula) y devuelva su precio. Si la lista de precios es monitor: 200€, teclado: 20€ ratón: 10€, al pedir precio('monitor') me devolverá 200.
---
+++ objetos.js
@@ -0,0 +1,7 @@
+// 1.- Crea un objeto llamado citizen1 y dale las propiedades name, lastname y age. Elige tú los valores.
+
+// 2.- Una vez creado, añádele la propiedad city, con el valor que tú quieras, y cambia el valor de la propiedad age. Comprueba que los cambios han surtido efecto.
+
+// 3.- Crea una función checkCitizenship que acepte como parámetro un objeto, compruebe la edad del ciudadano y, si ésta es mayor de 18, añada la propiedad canVote: true; de lo contrario, debe añadir canVote: false. Usala con el objeto citizen1.
+
+// 4.- Crea una función llamada createCitizen que acepte 3 strings: nombre, apellido y edad. Si los parámetros son válidos, debe retornar un nuevo objeto y almacenarlos en un array.
