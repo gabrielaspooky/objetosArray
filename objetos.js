fadf076781e852aa530bc7492511c0efda13435b
Gabriela <54961860+gabrielaspooky@users.noreply.github.com>
2024-05-06 11:32:01 +0200

    added new function

--- objetos.js
+++ objetos.js
@@ -1,20 +1,40 @@
 // 1.- Crea un objeto llamado citizen1 y dale las propiedades name, lastname y age. Elige tú los valores.
 
 let = citizen1 = {
-citizenName = 'Taylor'
-citizenLastName = 'Swift'
-citizenAge = 33
+citizenName: 'Taylor',
+citizenLastName: 'Swift',
+citizenAge: 33
 }
 
 // 2.- Una vez creado, añádele la propiedad city, con el valor que tú quieras, y cambia el valor de la propiedad age. Comprueba que los cambios han surtido efecto.
 
 let = citizen1 = {
-    citizenName = 'Taylor'
-    citizenLastName = 'Swift'
-    citizenAge = 34
-    city = 'West Reading'
+    citizenName: 'Taylor',
+    citizenLastName: 'Swift',
+    citizenAge: 34,
+    city: 'West Reading'
     }
 
 // 3.- Crea una función checkCitizenship que acepte como parámetro un objeto, compruebe la edad del ciudadano y, si ésta es mayor de 18, añada la propiedad canVote: true; de lo contrario, debe añadir canVote: false. Usala con el objeto citizen1.
+
+function checkCitizenship(citizen1) {
+    let = citizen1 = {
+        citizenName: 'Taylor',
+        citizenLastName: 'Swift',
+        citizenAge: 34,
+        city: 'West Reading'
+        }
+if (citizen1.citizenAge >= 18) {
+    citizen1.canVote = true;
+}
+ else {
+    citizen1.canVote = false;
+ }
+ return;
+}
+
+checkCitizenship();
+
+console.log(citizen1);
 
 // 4.- Crea una función llamada createCitizen que acepte 3 strings: nombre, apellido y edad. Si los parámetros son válidos, debe retornar un nuevo objeto y almacenarlos en un array.
