b339914230e0c3fdf0556c794c080f6e4a1182d5
Gabriela <54961860+gabrielaspooky@users.noreply.github.com>
2024-05-06 12:01:35 +0200

    added another function

--- objetos.js
+++ objetos.js
@@ -38,3 +38,28 @@ checkCitizenship();
 console.log(citizen1);
 
 // 4.- Crea una función llamada createCitizen que acepte 3 strings: nombre, apellido y edad. Si los parámetros son válidos, debe retornar un nuevo objeto y almacenarlos en un array.
+
+let citizens = []; 
+
+function createCitizen() {
+   
+    let name = prompt("Introduce tu nombre:");
+    let lastName = prompt("Introduce tu apellido:");
+    let age = prompt("Introduce tu edad:");
+
+    if (typeof name === 'string' && typeof lastName === 'string' && !isNaN(age)) {
+        let newCitizen = {
+            name: name,
+            lastName: lastName,
+            age: age
+        };
+        citizens.push(newCitizen); 
+
+        alert(`La ciudadanía de ${name} ${lastName}, con edad de ${age} años, ¡se ha creado exitosamente!`);
+    } else {
+        
+        return alert("Datos inválidos");
+    }
+}
+
+createCitizen();
